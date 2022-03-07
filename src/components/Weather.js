e527d8e86a4046d6644e12d24c721fd7b5585fda
lexbaron <brayanbaron000@gmail.com>
2022-03-07 17:49:03 -0500

    second commit

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,6 +4,8 @@ import axios from 'axios'
 const Weather = () => {
 
     const[weather, setWeather] = useState({})
+    const[temp, setTemp] = useState(0)
+    const[isCent, setIsCent] = useState(true)
 
     const success = (pos)=>{
         let latitude = pos.coords.latitude
@@ -11,34 +13,44 @@ const Weather = () => {
 
         axios.get( `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=b73597295a330aec76ce6845408772f9`)
     .then(res => {setWeather(res.data)
-    console.log(res.data)})
-
-
+                setTemp(Math.round((res.data?.main.temp) - 273.15))
+                
+                console.log(res.data)})
     }
 
     useEffect(() => {
-
         navigator.geolocation.getCurrentPosition(success)
-
     }, [])
 
-    
-
-    
-
-    
+    const convertTemp = () => {
+        if(isCent){
+            setTemp(Math.round((temp * 9/5) + 32))
+            
+            setIsCent(false)
+        }else{
+            setTemp(Math.round((temp - 32) * 5/9))
+            
+            setIsCent(true)
+        }
+    }
 
     return (
         <div className='weatherCard'>
-            <h1>WEATHER APP</h1>
-            <h4>{weather.name}, {weather.sys?.country}</h4>
-            <img src= {`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}@2x.png`} alt="" />
-            <p>{weather.main?.temp}°C</p>
-            <ul>
-                <li><b>wind speed:</b> {weather.wind?.speed}m/s</li>
-                
-            </ul>
-            
+            <div className='tittle'>
+                <h1>WEATHER APP</h1>
+                <h4>{weather.name}, {weather.sys?.country}</h4>
+            </div>
+            <div>
+                <img className='img' src={weather.weather?.[0].icon !== undefined ?`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}@2x.png` : console.log("not yet") } alt="ups" />
+                <p>{temp} {isCent ? "°C" : "°F"}  </p>
+            </div>
+            <div>
+                <ul>
+                    <li><b>wind speed:</b> {weather.wind?.speed}m/s</li>
+                    
+                </ul>
+            </div>
+            <button onClick={convertTemp}> <b>change to: </b> {isCent ? "Farenheit" : "Celsius"} </button>      
         </div>
     );
 };
